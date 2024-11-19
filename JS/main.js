b22f4bc9153b227c7f850d7ae75023cac85c3216
unknown <selenazuazo2008@cesanrafael.org>
2024-11-19 14:21:56 -0400

    Proyecto Selena

---
+++ JS/main.js
@@ -0,0 +1,46 @@
+let mensaje="Hola Mundo"
+console.log(mensaje)
+let numeroUno=2
+let numeroDos=4
+let numeroTres=10
+let resultado1=(numeroUno+numeroDos+numeroTres)
+console.log("la suma es:",resultado1)
+let resultado2=(numeroUno-numeroDos-numeroTres)
+console.log("la resta es:",resultado2)
+let resultado3=(numeroUno*numeroDos*numeroTres)
+console.log("la multiplicación es:",resultado3)
+let resultado4=(numeroUno/numeroDos/numeroTres)
+console.log("la división es:",resultado4)
+function sumar(){
+    let numeroUno=2
+    let numeroDos=4
+    let numeroTres=10
+    let resultado=(numeroUno+numeroDos+numeroTres)
+    alert("la suma es"+resultado)
+}
+function restar(){
+    let numeroUno=2
+    let numeroDos=4
+    let numeroTres=10
+    let resultado=(numeroUno-numeroDos-numeroTres)
+    alert("la resta es"+resultado)
+}
+function multiplicar(){
+    let numeroUno=2
+    let numeroDos=4
+    let numeroTres=10
+    let resultado=(numeroUno*numeroDos*numeroTres)
+    alert("la multiplicación es"+resultado)
+}function dividir(){
+    let numeroUno=2
+    let numeroDos=4
+    let numeroTres=10
+    let resultado=(numeroUno/numeroDos/numeroTres)
+    alert("la división es"+resultado)
+}
+
+
+
+
+
+
